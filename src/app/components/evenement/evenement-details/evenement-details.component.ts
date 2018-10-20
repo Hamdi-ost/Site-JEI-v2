ce8597dbf49a0ff1e01c6e3b66405371b6dc5092
HAMDI <benyaflahhamdi2@gmail.com>
2018-10-20 22:28:18 +0100

    event detail with our content

--- src/app/components/evenement/evenement-details/evenement-details.component.ts
+++ src/app/components/evenement/evenement-details/evenement-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-/*import * as data from '../../../../assets/contenu.json';*/
+import * as data from '../../../../assets/contenu.json';
 import { ActivatedRoute } from '@angular/router';
 
 
